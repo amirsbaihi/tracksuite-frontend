7d00ca51efec0464062527bc109f9f9681244be5
amirsbaihi <82115345+amirsbaihi@users.noreply.github.com>
2023-07-24 08:57:36 +0200

    Fixed apollo backend URL

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "localhost:4000",
+    uri: process.env.BACKEND_URL+":4000",
     cache: new InMemoryCache(),
 });
 
